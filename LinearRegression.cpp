e9dfb4003a0f9a9c3e84c7a8dc942d268a7e020c
DG <torreto279@live.com>
2020-06-19 20:06:57 +0530

    Linear Regression Done

--- LinearRegression.cpp
+++ LinearRegression.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
@@ -17,10 +18,17 @@ public:
 	}
 
 	double sum(){
-		return accumulate(v.begin(), v.end(), 0);
+		double s = 0;
+		// for(int i = 0; i < n; i++){
+		// 	s += v[i];
+		// }
+		for (vector<double>::iterator i = v.begin(); i != v.end(); i++){
+			s += *i;
+		}
+		return s;
 	}
 
-	double operator[](int& x){
+	double operator[](int x){
 		return v[x];
 	}
 
@@ -38,6 +46,14 @@ public:
 		}
 	}
 
+	Vect operator+(double x){
+		Vect v2;
+		for(int i = 0; i < this->length(); i++){
+			v2.append(v[i] + x);
+		}
+		return v2;
+	}
+
 	Vect operator-(Vect& v1){
 		if (this->length() != v1.length()){
 			cout << "Vectors are not equal in length";
@@ -52,16 +68,38 @@ public:
 		}
 	}
 
+	Vect operator*(Vect v1){
+		if (this->length() != v1.length()){
+			cout << "Vectors are not equal in length";
+			exit(1);
+		}
+		else{
+			Vect v2;
+			for(int i = 0; i < this->length(); i++){
+				v2.append(v[i] * v1[i]);
+			}
+			return v2;
+		}
+	}
+
 	Vect operator*(double x){
 		Vect v2;
-		for (std::vector<double>::iterator i = v.begin(); i != v.end(); i++){
+		for (vector<double>::iterator i = v.begin(); i != v.end(); i++){
 			v2.append((*i)*x);
 		}
 		return v2;
 	}
 
+	/**/Vect square(){
+		Vect v2;
+		for (vector<double>::iterator i = v.begin(); i != v.end(); i++){
+			v2.append((*i)*(*i));
+		}
+		return v2;
+	}
+
 	/**/void disp(){
-		for (std::vector<double>::iterator i = v.begin(); i != v.end(); i++){
+		for (vector<double>::iterator i = v.begin(); i != v.end(); i++){
 			cout << *i << " ";
 		}
 		cout << "\n";
@@ -72,6 +110,36 @@ public:
 
 
 int main(){
+	const double eps = 1e-6, alp = 0.01;	//Error, Epsilon and Alpha
+	double err = 1, temp;
+	int iter = 0;
 	
+	double w1 = 0, w0 = 0;
+	Vect x, y;
+	for (int i = 0; i < 10; ++i){
+		x.append(i);
+	}
+	y = x*5 + 3;	//Ground truth
+
+	Vect J, yh;	//Loss funcion and the hypothesis function
+	yh = ((x*w1) + w0);
+	J.append((((y - yh).square()).sum())/20);
+
+	while (err > eps && iter < 5000){
+		double delJ0 = -((y - yh).sum())/10;
+		double delJ1 = -(((x*(y - yh)).sum()))/10;
+
+		w0 -= alp*delJ0;
+		w1 -= alp*delJ1;
+
+		iter++;
+		yh = ((x*w1) + w0);
+		J.append((((y - yh).square()).sum())/20);
+		temp = J[iter] - J[iter-1];
+		err = (temp > 0) ? temp : -temp;
+
+	}
+
+	cout << J[iter] << " " << w1 << " " << w0 << " " << iter << " " << err;
 
 }
