f3e83b413c126bd1d9fda2d0fa718faaa5fa4720
DG <gchakradhar72@gmail.com>
2021-04-18 00:03:43 +0530

    Added time measurement and gitignore

--- PolynomialRegression.cpp
+++ PolynomialRegression.cpp
@@ -7,6 +7,7 @@ in order to get a more accurate output.
 #include <vector>
 #include <cmath>
 #include <time.h>
+#include <chrono>
 
 using namespace std;
 
@@ -131,12 +132,20 @@ void readData(int&, Vect&, Vect&, int&, double&);
 void writeData(Vect&, Vect&, Vect&, int&);
 
 
+void elapsedTime(chrono::_V2::system_clock::time_point st, chrono::_V2::system_clock::time_point en, string s){
+	chrono::duration <float> dur = en - st;
+	cout << "\n" << s << dur.count() * 1000 << " ms";
+}
+
 int main(){
+	auto t0 = chrono::high_resolution_clock::now();
 	double err = 1, eps = 1e-6, alp;	//^eps = stopping criterion epsilon, alp = learning rate alpha
 	int iter = 0, order, len, denom, epochs = 10000;	//^
 	Vect x, y, yh, J;	//x,y = data points, yh = hypothesis function, J = loss function
+	auto t1 = chrono::high_resolution_clock::now();
 
 	readData(len, x, y, order, alp);
+	auto t2 = chrono::high_resolution_clock::now();
 
 	/*This block is to get the inputs from the terminal (user)*/
 	// cout << "Length of the vector? ";
@@ -156,6 +165,7 @@ int main(){
 	denom = 10 * len;	//^
 	alp = 5 * pow(10, -order);	//Comment to give custom learning rate at runtime
 	order++;
+	auto t3 = chrono::high_resolution_clock::now();
 
 	double* W = new double[order];	//Weight matrix
 	double* delJ = new double[order];	//Partial derivatives of J
@@ -163,6 +173,7 @@ int main(){
 	for (int i = 0; i < order; i++){	//Initializing weights randomly
 		W[i] = ((double) rand()/(RAND_MAX));
 	}
+	auto t4 = chrono::high_resolution_clock::now();
 
 	yh = (x^0) * W[0];
 	for (int i = 1; i < order; i++){
@@ -171,6 +182,7 @@ int main(){
 	
 	J.append(((y - yh)^2).sum()/(2*len));
 
+	auto t5 = chrono::high_resolution_clock::now();
 	while (err > eps && iter < epochs){
 		for (int i = 0; i < order; i++){
 			delJ[i] = -(((x^i)*(y - yh)).sum())/denom;
@@ -191,8 +203,17 @@ int main(){
 		err = (err < 0) ? -err : err;
 
 	}
+	auto t6 = chrono::high_resolution_clock::now();
 
-	writeData(x, y, yh, len);
+	elapsedTime(t0, t1, "Initialization: ");
+	elapsedTime(t1, t2, "Read Data: ");
+	elapsedTime(t2, t3, "2nd Initialization: ");
+	elapsedTime(t3, t4, "Weights: ");
+	elapsedTime(t4, t5, "1st Iteration: ");
+	elapsedTime(t5, t6, "Bulk: ");
+	elapsedTime(t0, t6, "\nTotal: ");
+
+	// writeData(x, y, yh, len);
 
 	/*This block is to print the output to the terminal*/
 	// cout << "\n" << err << " " << iter << "\n";
